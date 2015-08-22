bcdd2b3a7575e6330d31c870b9d9461b01cf0951
Ilya Stepanov <dev@ilyastepanov.com>
2015-08-22 14:10:57 -0700

    Fix empty cron env vars

--- start.sh
+++ start.sh
@@ -8,6 +8,10 @@ PREFIX=${PREFIX:-backup}
 if [[ "$1" == 'no-cron' ]]; then
     exec /archive.sh
 else
-    echo -e "PREFIX=$PREFIX\nTAR_PARAMS=$TAR_PARAMS\n$CRON_SCHEDULE /archive.sh" | crontab -
+    CRON_ENV="PREFIX=$PREFIX"
+    if [ -n "$TAR_PARAMS" ]; then
+        CRON_ENV="$CRON_ENV\nTAR_PARAMS=$TAR_PARAMS"
+    fi
+    echo -e "$CRON_ENV\n$CRON_SCHEDULE /archive.sh" | crontab -
     exec cron -f
 fi
