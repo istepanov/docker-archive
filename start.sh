1265a51b487f36dc8d96c47ed1816c3b9c3d2f38
Ilya Stepanov <dev@ilyastepanov.com>
2015-09-14 18:32:01 -0700

    Use FIFO for logging

--- start.sh
+++ start.sh
@@ -8,13 +8,16 @@ PREFIX=${PREFIX:-backup}
 if [[ "$1" == 'no-cron' ]]; then
     exec /archive.sh
 else
+    LOGFIFO='/var/log/cron.fifo'
+    if [[ ! -e "$LOGFIFO" ]]; then
+        mkfifo "$LOGFIFO"
+    fi
     CRON_ENV="PREFIX=$PREFIX"
     if [ -n "$TAR_PARAMS" ]; then
         CRON_ENV="$CRON_ENV\nTAR_PARAMS=$TAR_PARAMS"
     fi
-    touch /var/log/cron.log
-    echo -e "$CRON_ENV\n\n$CRON_SCHEDULE /archive.sh >> /var/log/cron.log 2>&1"
-    echo -e "$CRON_ENV\n\n$CRON_SCHEDULE /archive.sh >> /var/log/cron.log 2>&1" | crontab -
+    echo -e "$CRON_ENV\n\n$CRON_SCHEDULE /archive.sh > $LOGFIFO 2>&1"
+    echo -e "$CRON_ENV\n\n$CRON_SCHEDULE /archive.sh > $LOGFIFO 2>&1" | crontab -
     cron
-    tail -f /var/log/cron.log
+    tail -f "$LOGFIFO"
 fi
