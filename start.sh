adc4e5470a26863e3a7e7ab833f858316883c64c
Ilya Stepanov <dev@ilyastepanov.com>
2015-09-15 21:39:29 -0700

    Cleanup

--- start.sh
+++ start.sh
@@ -16,8 +16,8 @@ else
     if [ -n "$TAR_PARAMS" ]; then
         CRON_ENV="$CRON_ENV\nTAR_PARAMS=$TAR_PARAMS"
     fi
-    echo -e "$CRON_ENV\n\n$CRON_SCHEDULE /archive.sh > $LOGFIFO 2>&1"
     echo -e "$CRON_ENV\n\n$CRON_SCHEDULE /archive.sh > $LOGFIFO 2>&1" | crontab -
+    crontab -l
     cron
     tail -f "$LOGFIFO"
 fi
